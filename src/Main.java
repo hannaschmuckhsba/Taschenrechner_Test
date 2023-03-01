aa0fae179cf7f36a8ca1b5b7973064d838afd7d3
HannaSchmuck <hannaschmuck123@gmail.com>
2023-03-01 13:30:15 +0100

    Funktion: Addieren

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Taschenrechner_Backend calculator = new Taschenrechner_Backend();
+
+                double result = calculator.add(3,5);
+                System.out.println("Das Ergebnis ist: "+result);
+    }
+}
---
+++ src/Taschenrechner_Backend.java
@@ -0,0 +1,6 @@
+public class Taschenrechner_Backend {
+
+    public double add (double a, double b) {
+        return a+b;
+    }
+}
